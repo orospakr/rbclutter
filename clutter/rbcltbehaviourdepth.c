91d908405fbd898425560f3aece1ba396ca64091
Neil Roberts <neil@openedhand.com>
2008-05-01 20:11:39 +0100

    * clutter/rbcltbehaviourdepth.c: Added wrappers for clutter_behaviour_depth_{get,set}_bounds.

--- clutter/rbcltbehaviourdepth.c
+++ clutter/rbcltbehaviourdepth.c
@@ -1,5 +1,5 @@
 /* Ruby bindings for the Clutter 'interactive canvas' library.
- * Copyright (C) 2007  Neil Roberts
+ * Copyright (C) 2007-2008  Neil Roberts
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -24,7 +24,10 @@
 #include "rbclutter.h"
 
 static VALUE
-rbclt_behaviour_depth_initialize (VALUE self, VALUE alpha, VALUE start_depth, VALUE end_depth)
+rbclt_behaviour_depth_initialize (VALUE self,
+				  VALUE alpha,
+				  VALUE start_depth,
+				  VALUE end_depth)
 {
   ClutterBehaviour *behaviour;
 
@@ -37,10 +40,36 @@ rbclt_behaviour_depth_initialize (VALUE self, VALUE alpha, VALUE start_depth, VA
   return Qnil;
 }
 
+static VALUE
+rbclt_behaviour_depth_set_bounds (VALUE self,
+				  VALUE depth_start,
+				  VALUE depth_end)
+{
+  ClutterBehaviourDepth *behaviour = CLUTTER_BEHAVIOUR_DEPTH (RVAL2GOBJ (self));
+  clutter_behaviour_depth_set_bounds (behaviour,
+				      NUM2INT (depth_start),
+				      NUM2INT (depth_end));
+  return self;
+}
+
+static VALUE
+rbclt_behaviour_depth_get_bounds (VALUE self)
+{
+  ClutterBehaviourDepth *behaviour = CLUTTER_BEHAVIOUR_DEPTH (RVAL2GOBJ (self));
+  gint depth_start, depth_end;
+
+  clutter_behaviour_depth_get_bounds (behaviour, &depth_start, &depth_end);
+
+  return rb_ary_new3 (2, INT2NUM (depth_start), INT2NUM (depth_end));
+}
+
 void
 rbclt_behaviour_depth_init ()
 {
-  VALUE klass = G_DEF_CLASS (CLUTTER_TYPE_BEHAVIOUR_DEPTH, "BehaviourDepth", rbclt_c_clutter);
+  VALUE klass = G_DEF_CLASS (CLUTTER_TYPE_BEHAVIOUR_DEPTH,
+			     "BehaviourDepth", rbclt_c_clutter);
 
   rb_define_method (klass, "initialize", rbclt_behaviour_depth_initialize, 3);
+  rb_define_method (klass, "set_bounds", rbclt_behaviour_depth_set_bounds, 2);
+  rb_define_method (klass, "get_bounds", rbclt_behaviour_depth_get_bounds, 0);
 }
