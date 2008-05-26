cdcca84820f31720efabbaca08b8942a2eae83b8
Neil Roberts <neil@openedhand.com>
2008-05-26 15:24:57 +0100

    * clutter/rbcltevent.c.in: * clutter/mkrbcltevent.rb: The root 'Event' class is now created in mkrbcltevent.rb because it now has a lot of fields so the getters and setters are created automatically. The common fields from the other classes (such as 'time') have been removed. All classes have been updated to version 0.7

--- clutter/mkrbcltevent.rb
+++ clutter/mkrbcltevent.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 
 # Ruby bindings for the Clutter 'interactive canvas' library.
-# Copyright (C) 2007  Neil Roberts
+# Copyright (C) 2007-2008  Neil Roberts
 # 
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -17,11 +17,15 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA  02110-1301  USA
+
 class EventStruct
-  attr_reader :name, :fields, :default_type, :types
+  attr_reader :name, :class_name, :super_class_name;
+  attr_reader :fields, :default_type, :types
 
   def initialize(name, default_type, types, *fields)
     @name = name
+    @class_name = name.sub(/\AClutter/, "")
+    @super_class_name = "rbclt_event_class"
     @default_type = default_type
     @fields = {}
     @types = types
@@ -32,36 +36,57 @@ class EventStruct
     end
   end
 
+  def set_class_name(class_name)
+    @class_name = class_name
+    self
+  end
+
+  def set_super_class_name(super_class_name)
+    @super_class_name = super_class_name
+    self
+  end
+
   def var_name
-    name_parts = @name.split(/(?=[A-Z])/)
+    name_parts = @class_name.split(/(?=[A-Z])/)
     name_parts.slice!(0) if name_parts[0] == "Clutter"
     name_parts.map { |part| part.downcase }.join("_")
   end
 end
 
-EVENT_STRUCTS = [ 
+EVENT_STRUCTS = [
+  EventStruct.new("ClutterAnyEvent", "CLUTTER_NOTHING",
+                  %w{ CLUTTER_NOTHING CLUTTER_DESTROY_NOTIFY
+                      CLUTTER_DELETE CLUTTER_CLIENT_MESSAGE },
+                  "time", "guint32",
+                  "flags", "ClutterEventFlags",
+                  "stage", "gobject",
+                  "source", "gobject") \
+                 .set_class_name("Event") \
+                 .set_super_class_name(nil),
   EventStruct.new("ClutterKeyEvent", "CLUTTER_KEY_PRESS",
                   %w{ CLUTTER_KEY_PRESS CLUTTER_KEY_RELEASE },
-                  "time", "guint32",
                   "modifier_state", "ClutterModifierType",
                   "keyval", "guint",
                   "hardware_keycode", "guint16"),
   EventStruct.new("ClutterButtonEvent", "CLUTTER_BUTTON_PRESS",
                   %w{ CLUTTER_BUTTON_PRESS CLUTTER_BUTTON_RELEASE },
-                  "time", "guint32",
                   "x", "gint",
                   "y", "gint",
                   "modifier_state", "ClutterModifierType",
-                  "button", "guint32"),
+                  "button", "guint32",
+                  "click_count", "guint"),
+  EventStruct.new("ClutterCrossingEvent", "CLUTTER_ENTER",
+                  %w{ CLUTTER_ENTER CLUTTER_LEAVE },
+                  "x", "gint",
+                  "y", "gint",
+                  "related", "gobjectr"),
   EventStruct.new("ClutterMotionEvent", "CLUTTER_MOTION",
                   %w{ CLUTTER_MOTION },
-                  "time", "guint32",
                   "x", "gint",
                   "y", "gint",
                   "modifier_state", "ClutterModifierType"),
   EventStruct.new("ClutterScrollEvent", "CLUTTER_SCROLL",
                   %w{ CLUTTER_SCROLL },
-                  "time", "guint32",
                   "x", "gint",
                   "y", "gint",
                   "direction", "ClutterScrollDirection",
@@ -88,6 +113,8 @@ def cval2rbval(cval, field_type)
     "INT2NUM (#{cval})"
   when /\AClutter/
     "GENUM2RVAL (#{cval}, #{get_type_name(field_type)})"
+  when "gobject", "gobjectr"
+    "GOBJ2RVAL (#{cval})"
   else
     STDERR.print("Unknown type #{field_type}\n")
     exit(1)
@@ -104,19 +131,36 @@ def rbval2cval(rbval, field_type)
     "rbclt_num_to_guint16 (#{rbval})"
   when /\AClutter/
     "RVAL2GENUM (#{rbval}, #{get_type_name(field_type)})"
+  when "gobject"
+    "RVAL2GOBJ (#{rbval})"
   else
     STDERR.print("Unknown type #{field_type}\n")
     exit(1)
   end
 end
 
+def set_cval(cvar, rbval, field_type)
+  if field_type == "gobjectr"
+    "{\n" \
+    "  gpointer _old_val = #{cvar};\n" \
+    "  if ((#{cvar} = RVAL2GOBJ (#{rbval})))\n" \
+    "    g_object_ref (#{cvar});\n" \
+    "  if (_old_val)\n" \
+    "    g_object_unref (_old_val);\n" \
+    "}"
+  else
+    "#{cvar} = #{rbval2cval(rbval, field_type)}"
+  end
+end
+
 def make_event_functions(out_file)
   EVENT_STRUCTS.each do |event|
+    out_file.print("static VALUE rbclt_#{event.var_name}_class;\n" \
+                   "\n")
+
     event.fields.each_pair do |field_name, field_type|
       cval = "((#{event.name} *) RVAL2BOXED (self, CLUTTER_TYPE_EVENT))->#{field_name}"
-      out_file.print("static VALUE rbclt_#{event.var_name}_class;\n" \
-                     "\n" \
-                     "static VALUE\n" \
+      out_file.print("static VALUE\n" \
                      "rbclt_#{event.var_name}_get_#{field_name} (VALUE self)\n" \
                      "{\n" \
                      "  return #{cval2rbval(cval, field_type)};\n" \
@@ -125,21 +169,34 @@ def make_event_functions(out_file)
                      "static VALUE\n" \
                      "rbclt_#{event.var_name}_set_#{field_name} (VALUE self, VALUE value)\n" \
                      "{\n" \
-                     "  #{cval} = #{rbval2cval('value', field_type)};\n" \
+                     "  #{set_cval(cval, 'value', field_type)};\n" \
                      "  return self;\n" \
                      "}\n" \
                      "\n")
     end
+
+    event_type_check = event.types.map { |x| "event.type != #{x}" }.join(" && ")
     
     out_file.print("static VALUE\n" \
-                   "rbclt_#{event.var_name}_initialize (int argc, VALUE *argv, VALUE self)\n" \
+                   "rbclt_#{event.var_name}_initialize (int argc,\n" \
+                   "                                    VALUE *argv,\n" \
+                   "                                    VALUE self)\n" \
                    "{\n" \
                    "  VALUE event_type;\n" \
                    "  ClutterEvent event;\n" \
                    "  rb_scan_args (argc, argv, \"01\", &event_type);\n" \
                    "  memset (&event, '\\0', sizeof (event));\n" \
-                   "  event.type = event_type == Qnil ? #{event.default_type}\n" \
-                   "    : RVAL2GENUM (event_type, CLUTTER_TYPE_EVENT_TYPE);\n" \
+                   "  if (event_type == Qnil)\n" \
+                   "    event.type = #{event.default_type};\n" \
+                   "  else\n" \
+                   "    {\n" \
+                   "      event.type\n" \
+                   "        = RVAL2GENUM (event_type,\n" \
+                   "                      CLUTTER_TYPE_EVENT_TYPE);\n" \
+                   "      if (#{event_type_check})\n" \
+                   "        rb_raise (rb_eArgError,\n" \
+                   "                  \"invalid event number\");\n" \
+                   "    }\n" \
                    "  G_INITIALIZE (self, &event);\n" \
                    "  return Qnil;\n" \
                    "}\n" \
@@ -179,9 +236,12 @@ end
 
 def make_class_defs(out_file)
   EVENT_STRUCTS.each do |event|
-    class_name = event.name.sub(/\AClutter/, "")
-    out_file.print("  klass = rb_define_class_under (rbclt_c_clutter, " \
-                   "\"#{class_name}\", rbclt_event_class);\n")
+    if event.super_class_name
+      out_file.print("  klass = rb_define_class_under (rbclt_c_clutter, " \
+                     "\"#{event.class_name}\", #{event.super_class_name});\n")
+    else
+      out_file.print("  klass = rbclt_#{event.var_name}_class;\n")
+    end
     event.fields.each_pair do |field_name, field_type|
       out_file.print("  rb_define_method (klass, \"#{field_name}\", " \
                      "rbclt_#{event.var_name}_get_#{field_name}, 0);\n" \
