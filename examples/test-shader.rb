1004a1263e051f5ef2cd2907aa8684e24dfd4883
Neil Roberts <neil@openedhand.com>
2008-07-04 09:25:57 +0100

    * examples/test-shader.rb: Use sampler2D instead of sampler2DRect now that Clutter has removed support for the ARB_texture_rectangle extension.

--- examples/test-shader.rb
+++ examples/test-shader.rb
@@ -10,13 +10,15 @@ Clutter::init
 # sample shaders
 
 # FRAGMENT_SHADER_BEGIN: generate boilerplate with a local vec4 color
-# already initialized, from a sampler2DRect in a variable tex.
+# already initialized, from a sampler2D in a variable tex.
 
 FRAGMENT_SHADER_BEGIN = <<EOF
-uniform sampler2DRect tex;
+uniform sampler2D tex;
+uniform float x_step, y_step;
+
 void main ()
 {
-  vec4 color = texture2DRect (tex, vec2 (gl_TexCoord[0].st));
+  vec4 color = texture2D (tex, vec2 (gl_TexCoord[0].st));
 EOF
 
 # FRAGMENT_SHADER_END: apply the changed color to the output buffer
@@ -43,22 +45,22 @@ EOF
 
    [ "box-blur",
      <<EOF
-vec4 get_rgba_rel(sampler2DRect tex, float dx, float dy)
+vec4 get_rgba_rel(sampler2D tex, float dx, float dy)
 {
-  return texture2DRect (tex, gl_TexCoord[0].st + vec2(dx,dy) * 2.0);
+  return texture2D (tex, gl_TexCoord[0].st + vec2(dx,dy) * 2.0);
 }
 
 #{FRAGMENT_SHADER_BEGIN}
   float count = 1.0;
-  color += get_rgba_rel (tex, -1.0, -1.0); count++;
-  color += get_rgba_rel (tex, -1.0,  0.0); count++;
-  color += get_rgba_rel (tex, -1.0,  1.0); count++;
-  color += get_rgba_rel (tex,  0.0, -1.0); count++;
-  color += get_rgba_rel (tex,  0.0,  0.0); count++;
-  color += get_rgba_rel (tex,  0.0,  1.0); count++;
-  color += get_rgba_rel (tex,  1.0, -1.0); count++;
-  color += get_rgba_rel (tex,  1.0,  0.0); count++;
-  color += get_rgba_rel (tex,  1.0,  1.0); count++;
+  color += get_rgba_rel (tex, -x_step, -y_step); count++;
+  color += get_rgba_rel (tex, -x_step,  0.0);    count++;
+  color += get_rgba_rel (tex, -x_step,  y_step); count++;
+  color += get_rgba_rel (tex,  0.0,    -y_step); count++;
+  color += get_rgba_rel (tex,  0.0,     0.0);    count++;
+  color += get_rgba_rel (tex,  0.0,     y_step); count++;
+  color += get_rgba_rel (tex,  x_step, -y_step); count++;
+  color += get_rgba_rel (tex,  x_step,  0.0);    count++;
+  color += get_rgba_rel (tex,  x_step,  y_step); count++;
   color = color / count;
 #{FRAGMENT_SHADER_END}
 EOF
@@ -98,7 +100,7 @@ EOF
    [ "combined-mirror",
      <<EOF
 #{FRAGMENT_SHADER_BEGIN}
-  vec4 colorB = texture2DRect (tex, vec2(gl_TexCoord[0].ts));
+  vec4 colorB = texture2D (tex, vec2(gl_TexCoord[0].ts));
   float avg = (color.r + color.g + color.b) / 3.0;
   color.r = avg;
   color.g = avg;
@@ -139,6 +141,9 @@ def set_shader(actor, new_no)
       actor.set_shader_param("radius", 3.0)
       actor.set_shader_param("brightness", 0.4)
       actor.set_shader_param("contrast", -1.9)
+
+      actor.set_shader_param("x_step", 1.0 / actor.width)
+      actor.set_shader_param("y_step", 1.0 / actor.height)
     end
   end 
 end
