0d6fd59f1276606d089d7ed4f99fb49f7c3d15cd
Andrew Clunis <andrew@orospakr.ca>
2009-07-29 19:00:56 -0400

    Removed clutter-cairo.

--- clutter-cairo/clutter_cairo.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/ruby
-
-# This is needed to ensure that clutter.so is loaded before
-# clutter_cairo.so so that it can reference symbols in clutter.so
-
-require 'clutter'
-require 'clutter_cairo.so'
--- extconf.rb
+++ extconf.rb
@@ -2,17 +2,17 @@
 
 # Ruby bindings for the Clutter 'interactive canvas' library.
 # Copyright (C) 2007  Neil Roberts
-# 
+#
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
-# 
+#
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -24,7 +24,7 @@ def show_fail
   exit(1)
 end
 
-SUBDIRS = %w{ clutter clutter-cairo clutter-gtk clutter-gst }
+SUBDIRS = %w{ clutter clutter-gtk clutter-gst }
 
 cwd = Pathname.getwd
 
